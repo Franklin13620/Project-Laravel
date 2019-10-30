5d3e82b8a7e51a5165a50213a045083c06e9ad26
Franklin13620 <franklintorres13620@gmail.com>
2019-10-29 20:14:25 -0600

    views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Bien, Estás conectado!
                 </div>
             </div>
         </div>
